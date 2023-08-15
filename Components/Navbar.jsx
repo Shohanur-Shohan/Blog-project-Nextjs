75942f0ccebbe295cd587cf3fa2c1740f65b4365
Shohanur-Shohan <shohanurshohan734@gmail.com>
2023-08-15 22:43:13 +0600

    navbar

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between px-10 mb-5 shadow-md '>
-            <div className='my-auto text-3xl'>Blog</div>
-            <div className='flex my-auto text-lg'>
+        <div className="flex justify-between px-10 mb-5 shadow-md">
+            <div className="my-auto text-3xl">Blog</div>
+            <div className="flex my-auto text-lg">
                 <Link className='m-5' href='/'>Home</Link>
                 <Link className='m-5' href='/blog'>Blog</Link>
                 <Link className='m-5' href='/post'>Post</Link>
