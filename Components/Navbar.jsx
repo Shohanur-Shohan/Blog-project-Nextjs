6fd22efd868f5adbf94e1a24be008b06f6d95b96
Shohanur-Shohan <shohanurshohan734@gmail.com>
2023-08-16 15:38:54 +0600

    added github

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <div className="flex justify-between p-5 mb-5 shadow-md">
+        <div className="flex justify-between px-10 py-5 mb-5 shadow-md">
             <div className="my-auto text-3xl">Blog</div>
             <div className="flex my-auto text-lg">
                 <Link className="ml-5" href='/'>Home</Link>
