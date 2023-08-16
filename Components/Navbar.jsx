7b3b784a49494c76b9e323333572f84a79824156
Shohanur-Shohan <shohanurshohan734@gmail.com>
2023-08-16 15:50:16 +0600

    navbar not styling

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,14 +1,15 @@
 
 import Link from 'next/link';
+import style from './Navbar.module.css'
 
 export default function Navbar() {
     return (
         <div className="flex justify-between px-10 py-5 mb-5 shadow-md">
             <div className="my-auto text-3xl">Blog</div>
             <div className="flex my-auto text-lg">
-                <Link className="ml-5" href='/'>Home</Link>
-                <Link className="ml-5" href='/blog'>Blog</Link>
-                <Link className="ml-5" href='/post'>Post</Link>
+                <Link className={style.link} href='/'>Home</Link>
+                <Link className={style.link} href='/blog'>Blog</Link>
+                <Link className={style.link} href='/post'>Post</Link>
             </div>
         </div>
     );
