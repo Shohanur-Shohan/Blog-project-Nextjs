806421af4b7f793f8e385f5afb511b9d050993b6
Shohanur-Shohan <shohanurshohan734@gmail.com>
2023-08-16 15:35:12 +0600

    added github

--- app/page.js
+++ app/page.js
@@ -11,6 +11,8 @@ export default function Home() {
         <Link href='/blog'>
           <button  className="gap-2 px-10 py-3 mt-5 text-sm font-semibold text-blue-500 transition-all bg-blue-100 border border-transparent rounded-md hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2">Click here</button>
         </Link>
+
+        <a href='https://github.com/Shohanur-Shohan/Blog-project-Nextjs' className='block mt-5 underline'>https://github.com/Shohanur-Shohan/Blog-project-Nextjs</a>
       </div>
     </div>   
     
